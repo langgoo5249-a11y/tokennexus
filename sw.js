f693c37026fd74325b2cddad13224d1b103a2975
TokenNexus Deploy <deploy@tokennexus.com>
2026-05-29 06:21:57 +0000

    update: 更新 Service Worker 文件 (zoneId: 11072282)

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "3nbf4.com",
-    "zoneId": 11072269
+    "zoneId": 11072282
 }
 self.lary = ""
 importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
